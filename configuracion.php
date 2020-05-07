aaae196177070351b3e649efb59ede195051d81d
JOM3C4 <56521920+JOM3C4@users.noreply.github.com>
2020-05-07 16:23:28 +0200

    version 1

---
+++ configuracion.php
@@ -0,0 +1,12 @@
+<?php
+$server  = "localhost";
+$user = "root";
+$pass = "";
+$bd = "LoginPHP";
+//$server  = "fdb24.awardspace.net" ;
+//$user = "3328815_loginphp";
+//$pass = "lolaso123";
+//$bd = "3328815_loginphp";
+
+
+?>
---
+++ main.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<link rel="stylesheet" href="estilo.css"  style="estilo.css">
+<meta charset="utf-8">
+<title>INSTIQUEST</title>
+<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+<meta name="viewport" content="width=device-width, user-scalable=no">
+
+</head>
+
+
+<body>
+
+<a id="whatsapp" href="https://api.whatsapp.com/send?phone=5491130160246"><img src="imagenes/whatsapp.png" alt=""></a>
+<header>
+  <div class="menu">
+    <div class="contenedor">
+      <p class="logo">INSTIQUEST</p>
+      <img class="menu-icon" src="imagenes/menu.png" alt="">
+      <nav>
+        <ul class="lista-menu">
+          <li><a href="#sobre-mi">Quienes Somos</a></li>
+          <li><a href="#servicios">Formación</a></li>
+          <li><a href="#mis-trabajos">Actividades</a></li>
+          <li><a href="#por-que-trabajar">Porque Nosotros</a></li>
+          <li><a href="index.php" target="_blank">Acceso Privado</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+
+<div class="contenedor siteheader">
+  <div class="contenedor-titulo">
+    <h1>Dando Lo Mejor de Nosotros</h1>
+    <h2>Formación Profesional y Secundaria</h2>
+    <a href="">Saber Más</a>
+  </div>
+</div>
+
+</header>
+
+
+<main>
+
+<section id="sobre-mi">
+  <div class="contenedor">
+    <h3>Sobre Nosotros</h3>
+    <div class="contenedor-sobremi">
+      <div class="computadoras">
+      </div>
+      <div class="texto">
+        <h4>Instituto InfoQuest comprometidos contigo</h4>
+        <p>
+          Instituto Infoquest pertenece a la red de unas de las mejores escuelas de educación en España. Esto nos permite compartir experiencias y construir un modelo educativo innovador para formar personas competentes y solidarias..</p>
+        <p>La escuela se inició tímidamente durante el curso 1968-1969, con un parvulario de 15 niños y niñas de cuatro años y una escuela de oficiales delineantes con 12 alumnos. En 1974 comenzó la construcción del edificio actual.
+
+        Desde los inicios, se impartieron enseñanzas para niños -EGB- y para adultos -Formación Profesional y alfabetización.
+
+        A medida que han ido pasando los años, la escuela ha evolucionado y se ha adaptado a los cambios que la sociedad ha vivido. Ha mejorado sus metodologías educativas y ha transformado los espacios y las herramientas necesarias para formar a nuestros alumnos para que puedan aportar su talento a la sociedad..</p>
+        <a href="" style="text-align: center;">Inicio</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="servicios">
+  <div class="contenedor">
+    <h3>Formación</h3>
+    <div class="contenedor-servicios">
+      <div class="servicio violeta">
+        <h4>DAM</h4>
+        <p><b>REQUISITOS:</b><br>
+          Estar en posesión del título de bachiller (también válido el título de Bachiller expedido tras cursar el antiguo B.U.P, Bachillerato unificado y polivalente.)
+        Estar en posesión de un Título de Técnico Superior o Técnico Especialista.
+        Haber superado la prueba de Acceso a Ciclos formativos de Grado Superior.
+        Estar en posesión de un Título de Técnico (Formación Profesional de Grado Medio).</p>
+      </div>
+      <div class="servicio celeste">
+        <h4>DAW</h4>
+        <p><b>REQUISITOS:</b><br>
+          Estar en posesión del título de bachiller (también válido el título de Bachiller expedido tras cursar el antiguo B.U.P, Bachillerato unificado y polivalente.)
+        Estar en posesión de un Título de Técnico Superior o Técnico Especialista.
+        Haber superado la prueba de Acceso a Ciclos formativos de Grado Superior.
+        Estar en posesión de un Título de Técnico (Formación Profesional de Grado Medio).</p>
+      </div>
+      <div class="servicio violeta">
+        <h4>SIMIX</h4>
+        <p><b>REQUISITOS:</b><BR><br>
+          Requisito de la edad: tener 18 años.
+          Requisitos académicos: estar en posesión del título de Graduado en Enseñanza Secundaria Obligatoria (GESO) o equivalente.
+          Otros títulos que se consideran “equivalentes” a la ESO.</p>
+      </div>
+      <div class="servicio celeste">
+        <h4>SECUNDARIA</h4>
+        <p><b>REQUISITOS:</b><BR>
+          Tener el certificado de calificación pedagógica o el certificado de aptitud pedagógica. Tener el título de licenciado en pedagogía o psicopedagogía o cualquier equivalente. Haber obtenido las siguientes titulaciones. Licenciatura de ciencias de la actividad física y del deporte. </p>
+      </div>
+      <div class="servicio violeta">
+        <h4>BACHILLERATO</h4>
+        <p><b>REQUISITOS:</b><br>
+        Podrán acceder a los estudios del Bachillerato los estudiantes que cumplan alguno de los siguientes requisitos:
+        Estar en posesión del título de graduado en Educación Secundaria Obligatoria *.
+         Estar en posesión del título de técnico Deportivo. ...
+        Estar en posesión del título de técnico de Artes Plásticas y Diseño</p>
+      </div>
+      <div class="servicio celeste">
+        <h4>PFI</h4>
+        <p><b>REQUISITOS:</b><br>
+           Pueden acceder a este programa de formación e inserción
+            (PFI) los jóvenes no ocupados que cumplan como mínimo
+           16 años de edad y como máximo 21 en el año de inicio del
+           programa y que no hayan obtenido el graduado en ESO</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="mis-trabajos">
+  <div class="contenedor">
+    <h3>Actividades</h3>
+    <div class="owl-carousel owl-theme">
+    <div class="item">
+      <p>Deportes </p>
+      </a>
+    </div>
+    <div class="item">
+      <p>Excursiones</p>
+      </a>
+    </div>
+    <div class="item">
+      <p>Salidas</p>
+      </a>
+    </div>
+    <div class="item">
+      <p>Visitas</p>
+      </a>
+    </div>
+    <div class="item">
+      <p>Graduación</p>
+      </a>
+    </div>
+    <div class="item">
+      <p>Actividades Infantil</p>
+      </a>
+    </div>
+</div>
+  </div>
+</section>
+
+
+<section id="por-que-trabajar">
+  <div class="contenedor">
+    <h3>¿Por que Nosotros?</h3>
+    <div class="contenedor-trabajar">
+      <div class="item">
+        <ul>
+          <li></li>
+          <li>EDUCACIÓN PERSONALIZADA</li>
+          <li>Instituto InfoQuest, Instituto concertado en Barcelona, se compromete con sus familias a ofrecerles una educación personalizada e integral para que cada uno de sus alumnos pueda alcanzar el máximo desarrollo de sus capacidades, forme su propio criterio y pueda tomar decisiones de forma libre y responsable.</li>
+        </ul>
+      </div>
+      <div class="item">
+        <ul>
+          <li></li>
+          <li>EXCELENCIA EDUCATIVA</li>
+          <li>Instituto InfoQuest promueve una educación integral, que atiende a todas las facetas de la persona, basada en una sólida formación académica, humana. Educamos a nuestros alumnos para que sean buenas personas y buenos profesionales</li>
+        </ul>
+      </div>
+      <div class="item">
+        <ul>
+          <li></li>
+          <li>ENGLISH PROJECT</li>
+          <li>Porque además de ser un instituto, nuestro centro tiene ya implantado un Programa de Inmersión Lingüística en Galway (Irlanda). Este proyecto permite a los alumnos mejorar el Listening & Speaking y perder el miedo a hablar inglés</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="blog">
+  <div class="contenedor">
+    <h3>Actividades y Organigrama</h3>
+    <div class="contenedor-publicaciones">
+      <div class="publicacion">
+        <img src="imagenes/blog1.jpg" alt="">
+        <div class="contenido">
+          <h4>Organigrama </h4>
+          <p>Poseemos un Organigrama, con una lista con nuestro profesorado, y lista de nuestros empleados genericos de nuestro centro.</p>
+          <a href="https://prezi.com/view/xqF0Y8KHnUD166lWTxrd/">Leer mas ➝</a>
+        </div>
+      </div>
+      <div class="publicacion">
+        <img src="imagenes/blog2.jpg" alt="">
+        <div class="contenido">
+          <h4>Actividades extraescolares</h4>
+          <p>las actividades extraescolares ofrecen la posibilidad de desarrollar aspectos lúdicos y de aprendizaje dentro del entorno escolar.</p>
+          <a href="">Saber Más➝</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="que-esperas">
+  <div class="contenedor">
+    <h3>¿Qué esperas para Contactarnos?</h3>
+    <a href="">CONTACTANOS</a>
+  </div>
+</section>
+
+<section id="contacto">
+  <?php
+      if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+
+      <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+
+      <?php
+      } else{
+      ?>
+  <div class="contenedor">
+    <h3>Contacto</h3>
+    <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+      <input type="text" placeholder="Nombre" name="nombre" required>
+      <input type="email" placeholder="Email" name="email" required>
+      <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+      <input type="submit" value="ENVIAR MENSAJE">
+    </form>
+          <?php } ?>
+
+
+    <div class="contacto-info">
+      <div class="mail"><img src="imagenes/mail-icon.png" alt="">InstiQuest@gmail.com</div>
+      <div class="whatsapp"><img src="imagenes/whatsapp-icon.png" alt="">63445252789</div>
+    </div>
+
+
+  </div>
+</section>
+
+</main>
+
+<footer>
+  <div class="contenedor">
+    <div class="redes">
+      <a href="#"><img src="imagenes/facebook.png" alt="imagen de facebook"></a>
+      <a href="#"><img src="imagenes/medium.png" alt="imagen de facebook"></a>
+      <a href="#"><img src="imagenes/github.png" alt="imagen de facebook"></a>
+      <a href="#"><img src="imagenes/linkedin.png" alt="imagen de facebook"></a>
+    </div>
+    <div class="parrafo">
+      <p>©InstiQuest | Instituto de Ciclo Formativos. Todos los derechos reservados</p>
+    </div>
+  </div>
+</footer>
+
+
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="owl/owl.carousel.min.js"></script>
+
+<!--Carrousel-->
+<!--<script>
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+</script>-->
+
+<script>
+
+$(document).ready(function(){
+
+$(window).scroll(function(){
+  scroll = $(window).scrollTop();
+  if (scroll > 100) {
+    $('.menu').css({"position":"fixed"});
+    $('.menu').css({"width":"100%"});
+    $('.menu').css({"top":"0"});
+    $('.menu').css({"background":"#fff"});
+    $('.menu a').css({"color":"#000"});
+    $('.logo').css({"color":"#000"});
+    $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+    $('.menu').css({"z-index":"100"});
+  } else {
+    $('.menu').css({"position":"relative"});
+    $('.menu').css({"background":"transparent"});
+    $('.menu').css({"box-shadow":"0 0 0"});
+    $('.menu a').css({"color":"#fff"});
+    $('.logo').css({"color":"#fff"});
+  }
+})
+
+$('.menu-icon').click(function(){
+  $('header nav').slideToggle();
+})
+
+})
+
+
+
+</script>
+
+
+</body>
+
+</html>
